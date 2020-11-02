81c5ce5e6d87a286611c40f6eec82d6374e190fd
xsx <3120909151@qq.com>
2020-11-02 14:22:46 +0800

    The first version

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xsx");
+		ArrayList<String> list = new ArrayList<String>();
+	    list.add("xsx");
+	    System.out.println(list);
+	}
+
+}
